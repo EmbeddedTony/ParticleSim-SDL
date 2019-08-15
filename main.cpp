bdc4b2883e11285ae3940384824012e2e6035559
TonyDaExpert <tonynin3ds@gmail.com>
2019-08-14 20:51:59 -0700

    Colors change slowly...

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <SDL.h>
 #include "Screen.h"
+#include <math.h>
 using namespace std;
 using namespace steinsgate;
 
@@ -15,17 +16,27 @@ int main(int argc, char* argv[]) {
 	
 
 
+
 	while (true) { 
 		// Update particles
 		// Draw particles
+		int elapsed =	SDL_GetTicks();
+		unsigned char green = (unsigned char)(( 1 + sin(elapsed * 0.0001)) * 128);
+		unsigned char red = (unsigned char)((1 + sin(elapsed * 0.0002)) * 128);
+		unsigned char blue = (unsigned char)((1 + sin(elapsed * 0.0003)) * 128);
+
+	
+
+	
+
 		for (int y = 0; y < Screen::SCREEN_HEIGHT; y++) {
 			for (int x = 0; x < Screen::SCREEN_WIDTH; x++) {
-				screen.setPixel(x, y, 128, 0, 255);
+				screen.setPixel(x, y, red, green, blue);
 			}
 
 		}
 	
-		screen.setPixel(450, 240, 255, 255, 255);
+	
 
 		//	Draw the screen
 		screen.update();
